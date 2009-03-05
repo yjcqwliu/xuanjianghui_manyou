ed38c3d9eaa2802a6e0c42f33a4db8fa5c7ce5c2
unknown <eagle@.(none)>
2009-03-05 12:01:05 +0800

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,18 @@
 require 'mongrel_cluster/recipes'
 
-set :application, "xuanjianghui"
+set :application, "xuanjianghuimy"
 set :scm, "?"
 set :repository,  "?"
 set :branch, "master"
 set :user, "?"
 set :use_sudo, false
 set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
-
+set :deploy_to, "/var/www/#{application}"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+# set :deploy_to, "/home/zhanglin/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
